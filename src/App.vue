ca0674dcf29443a7ba2889b4db5b666f8931ee3d
sandro <sandroazerty@icloud.com>
2022-01-29 23:05:56 +0100

    add typescript

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
 	<h1 class="text-3xl font-bold underline">Hello world!</h1>
